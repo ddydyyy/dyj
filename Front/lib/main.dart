6398224b72dc1e51cfa50de7a0168445ee344a90
ddydyyy <dy240319@gmail.com>
2024-10-05 01:51:48 +0900

    dy_241005_0150 test 주석 / 변수명 규칙 맞게 변경

--- Front/lib/main.dart
+++ Front/lib/main.dart
@@ -1,6 +1,6 @@
 // flutter의 기본적인 UI 구성 요소를 가져옴
 import 'package:flutter/material.dart';
-import 'Style/mainStyle.dart' as mainStyle;
+import 'Style/mainStyle.dart' as main_style;
 import 'Page/home.dart';
 
 // 실행 시 MyApp 클래스로 애플리케이션 실행.
@@ -9,7 +9,7 @@ void main() {
     // MaterialApp : 테마 등 여러 속성을 같이 시작해주는듯?
       MaterialApp(
         // Style/mainStyle.dart의 테마 상속받음
-          theme: mainStyle.AppTheme.theme,
+          theme: main_style.AppTheme.theme,
           home : Home()
       )
   );
