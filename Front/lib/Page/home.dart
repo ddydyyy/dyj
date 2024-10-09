f5f4964d0c286a475165000434ad0d7eea7c9fd3
ddydyyy <dy240319@gmail.com>
2024-10-09 14:46:44 +0900

    241009_1445 dy home 커스텀위젯 제작중

--- Front/lib/Page/home.dart
+++ Front/lib/Page/home.dart
@@ -1,42 +1,25 @@
-// flutter의 기본적인 UI 구성 요소를 가져옴
 import 'package:flutter/material.dart';
 
-// StatelessWidget을 상속받아, 앱의 기본 구조를 정의
+// StatelessWidget : 앱 시작 시 랜더링 후 불변
+// StatefulWidget : 각종 상호작용에 따라 UI 변경
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // 머티리얼 디자인을 사용하는 앱의 기본적인 설정을 제공
+
     return MaterialApp(
-      // 앱 제목
       title: 'Home',
-      // Scaffold 위젯 사용( 레이아웃 ), Wrapper 느낌
-      // 앱바( 헤더 ), 바디, BottomNavigationBar or persistentFooterButtons( footer )
       home: Scaffold(
-        // 앱바( 헤더 )
         appBar: AppBar(
-          // 제목
           title: const Text('Ref Demo'),
           titleTextStyle: Theme.of(context).textTheme.headlineLarge,
         ),
 
-        // Center 위젯 사용( 레이아웃, 중앙정렬 )
-        // const 쓰면 안바뀜 -> 최적화(다데서 갖다 써도 안바뀜)
-        // const -> Theme.of(context) 호출을 허용하지 않습니다.( 최대한 값을 유지하기 위해 참조를 최대한 못하게 막아둠 )
         body: ListView(
           // child : div, span 같은 느낌
-          children: [
-            TestWidget(
-              title: 'Hello, World!',
+          children: const [
+            ChartBox(
+              title: '어제 사람들이 관심있던 ETF/ETN',
               subtitle: 'This is a custom card widget.',
-            ),
-            const SizedBox(height: 20),
-            TestButtonWidget(
-              label: 'Click Me',
-              icon: Icons.thumb_up,
-              onPressed: () {
-                // Define the button action
-                print('Button Clicked!');
-              },
             ),
           ],
         ),
@@ -45,45 +28,60 @@ class Home extends StatelessWidget {
   }
 }
 
-class TestWidget extends StatelessWidget {
+class ChartBox extends StatelessWidget {
+  // 입력 변수 title, subtitle
   final String title;
   final String subtitle;
 
-  TestWidget({required this.title, required this.subtitle});
+  // TestWidget1 생성자
+  // required this.title, required this.subtitle
+  // -> 이 위젯 사용 시 title과 subtitle을 필수로 입력하도록 설정
+  const ChartBox({required this.title, required this.subtitle});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8), // Spacing between title and subtitle
-            Text(subtitle, style: const TextStyle(fontSize: 16)),
-          ],
-        ),
+    return Container(
+      color: Colors.black,
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        children: [
+          Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    this.title,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: (){ },
+                    child: Text('더보기')
+                  )
+                ],
+              ),
+            ],
+          ),
+
+        ],
       ),
     );
   }
 }
 
 
-class TestButtonWidget extends StatelessWidget {
-  final String label;
-  final IconData icon;
-  final VoidCallback onPressed;
-
-  TestButtonWidget({required this.label, required this.icon, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      icon: Icon(icon),
-      label: Text(label),
-    );
-  }
-}
+// class RankDetail extends StatelessWidget {
+//   RankDetail({required this.label, required this.icon, required this.onPressed});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton.icon(
+//       onPressed: onPressed,
+//       icon: Icon(icon),
+//       label: Text(label),
+//     );
+//   }
+// }
