d87f81a926005a26ffac9b9bcd02cb5a74b371c7
ddydyyy <dy240319@gmail.com>
2024-11-06 00:18:38 +0900

    241106_0000 dy home 제작중

--- Front/lib/Page/home.dart
+++ Front/lib/Page/home.dart
@@ -4,26 +4,119 @@ import '../CustomWidget/global.dart';
 // StatelessWidget : 앱 시작 시 랜더링 후 불변
 // StatefulWidget : 각종 상호작용에 따라 UI 변경
 class Home extends StatelessWidget {
+
+  // 다크모드 변수
+  final bool darkMode = false;
+
+
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       title: 'Home',
       home: Scaffold(
-        body: ListView(
-          // child : div, span 같은 느낌
-          children: [
-            SearchBox(
+        body: SingleChildScrollView(
+          child: Padding(
+            // 페이지 전체에 적용할 패딩
+            padding: const EdgeInsets.only(
+              left: 100,
+              right: 100,
+              top: 50,
+            ),
+            child: Column(
+              children: [
+                Column(
+                  // crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    // Column_1
+                    // 검색창
+                    Column(
+                      children: [
+                        // 아이콘 + 검색창
+                        const Row(
+                          children: [
+                            // 아이콘
+                            Icon(Icons.insert_emoticon),
+                            Padding(
+                                padding: EdgeInsets.only(
+                                  left: 10,
+                                ),
+                            ),
+                            Expanded(
+                              // TextField는 크기가 정해져 있지 않음
+                              // 왜인지 모르지만, 차지하는 공간이 계속 변하면서
+                              // 무한로딩되는 오류가 생기는듯
+                              // Expanded로 부모 위젯의 남은 공간을 모두 차지하게 해
+                              // 공간을 재할당하지 않게 해서 무한로딩 방지하는듯?
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: '주식, 메뉴, 상품, 뉴스를 검색하세요',
+                                  border: InputBorder.none, // 밑줄 제거
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        // 검색창 아래 밑줄
+                        Container(
+                          height: 2, // 밑줄의 두께
+                          color: Colors.grey, // 밑줄 색상
+                        ),
+                      ],
+                    ),
+
+
+
+
+
+
+                    Text(
+                      'Hey, Selena',
+                      style: TextStyle(
+                        color: darkMode? Colors.white : Colors.black,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                )
+              ],
 
             ),
-            const ChartBox(
-              title: '어제 사람들이 관심있던 ETF/ETN',
-              subtitle: 'This is a custom card widget.',
-            ),
-          ],
+          ),
         ),
       ),
     );
+
+
+
+    // return MaterialApp(
+    //   title: 'Home',
+    //   home: Scaffold(
+    //     body: ListView(
+    //       // child : div, span 같은 느낌
+    //       children: [
+    //         SearchBox(
+    //
+    //         ),
+    //         const ChartBox(
+    //           title: '어제 사람들이 관심있던 ETF/ETN',
+    //           subtitle: 'This is a custom card widget.',
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+
+
+
   }
 }
 
